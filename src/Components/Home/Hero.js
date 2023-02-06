f0af75810a6857ac97fbbd4fa8227828a7eda028
AbdurrahmanTalha <abdurrahmantalha.dev@gmail.com>
2023-02-06 23:59:50 +0600

    Added Hero.js responsive

--- src/Components/Home/Hero.js
+++ src/Components/Home/Hero.js
@@ -5,11 +5,13 @@ const Hero = () => {
     return (
         <div className="hero min-h-screen">
             <div className="hero-content flex-col lg:flex-row-reverse">
-                <img src={img} className="rounded-lg w-[50%]" alt="hero" />
-                <div className="w-[50%]">
-                    <h1 className="text-5xl font-bold">Be a <span className="text-[#ED4245]">Hero</span>, Donate Blood!</h1>
-                    <p className="py-6 text-1xl font-normal text-[#555]">Connect to Blood connects donors with local hospitals. Give the gift of life and help save lives in your community. Join us today.</p>
-                    <button className="btn btn-outline border-[#222222] border-2 hover:bg-[#ED4245] hover:border-[#ED4245]">Become a donar!</button>
+                <img src={img} className="rounded-lg md:w-[50%]" alt="hero" />
+                <div className="lg:w-[50%]">
+                    <h1 className="text-5xl font-bold lg:md:text-left text-center">Be a <span className="text-[#ED4245]">Hero</span>, Donate Blood!</h1>
+                    <p className="py-6 text-1xl font-normal text-[#555] lg:md:text-left text-center">Connect to Blood connects donors with local hospitals. Give the gift of life and help save lives in your community. Join us today.</p>
+                    <div className="lg:md:block flex justify-center items-center">
+                        <button className="btn btn-outline border-[#222222] border-2 hover:bg-[#ED4245] hover:border-[#ED4245] ">Become a donar!</button>
+                    </div>
                 </div>
             </div>
         </div>
