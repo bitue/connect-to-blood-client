f568514df2963fd6e0b83d9612f073a141986a6d
hrid620 <hridishere@gmail.com>
2023-02-06 23:44:00 +0600

    button not necessary

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import React from 'react';
+import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from './Pages/Home';
+import Home from "./Pages/Home";
 
 const App = () => {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <Home />,
-    }
+    },
   ]);
 
   return (
     <div className="bg-[#f1f1f1]">
       <RouterProvider router={router} />
+      <button className="btn btn-active">Button</button>
+      <button className="btn btn-active btn-primary">Button</button>
+      <button className="btn btn-active btn-secondary">Button</button>
+      <button className="btn btn-active btn-accent">Button</button>
+      <button className="btn btn-active btn-ghost">Button</button>
+      <button className="btn btn-active btn-link">Button</button>
     </div>
   );
 };
