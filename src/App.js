8a292991be283ba2bcb998aa8610031b5c1237ac
hrid620 <hridishere@gmail.com>
2023-02-07 11:21:58 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -13,12 +13,6 @@ const App = () => {
   return (
     <div className="bg-[#f1f1f1]">
       <RouterProvider router={router} />
-      <button className="btn btn-active">Button</button>
-      <button className="btn btn-active btn-primary">Button</button>
-      <button className="btn btn-active btn-secondary">Button</button>
-      <button className="btn btn-active btn-accent">Button</button>
-      <button className="btn btn-active btn-ghost">Button</button>
-      <button className="btn btn-active btn-link">Button</button>
     </div>
   );
 };
