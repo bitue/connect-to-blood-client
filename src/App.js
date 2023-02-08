1c358a843637c40bef5b2ba58678db466622ce4f
hrid620 <hridishere@gmail.com>
2023-02-09 00:04:04 +0600

    footer added by iqbal

--- src/App.js
+++ src/App.js
@@ -1,36 +1,38 @@
-import React from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Footer from "./Components/Shared/Footer";
 
-import Home from './Pages/Home';
-import Login from './Pages/Login';
-import Register from './Pages/Register';
-import RegisterDonar from './Pages/RegisterDonar';
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import RegisterDonar from "./Pages/RegisterDonar";
 
 const App = () => {
-    const router = createBrowserRouter([
-        {
-            path: '/',
-            element: <Home />
-        },
-        {
-            path: '/login',
-            element: <Login />
-        },
-        {
-            path: '/register',
-            element: <Register />
-        },
-        {
-            path: "/registerDonar",
-            element: <RegisterDonar />
-        }
-    ]);
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+    {
+      path: "/registerDonar",
+      element: <RegisterDonar />,
+    },
+  ]);
 
-    return (
-        <div>
-            <RouterProvider router={router} />
-        </div>
-    );
+  return (
+    <div>
+      <RouterProvider router={router} />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
