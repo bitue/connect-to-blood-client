b885d264515d6247fac95369719d2c686854c846
hrid620 <hridishere@gmail.com>
2023-02-08 23:42:22 +0600

    footer and some code changes

--- src/App.js
+++ src/App.js
@@ -16,6 +16,10 @@ const App = () => {
       element: <Login />,
     },
     {
+      path: "/register",
+      element: <Register />,
+    },
+    {
       path: "/registerDonar",
       element: <RegisterDonar />,
     },
@@ -29,54 +33,3 @@ const App = () => {
 };
 
 export default App;
-
-/**
- import React from "react";
-import sample from "../../Assets/images/blood-tube.png";
-const Footer = () => {
-  return (
-    <footer className="footer p-12 bg-[#f1f1f1] text-black-content mt-8  flex justify-between">
-      <div>
-        <img
-          src={sample}
-          width="50"
-          height="50"
-          viewBox="0 0 24 24"
-          fillRule="evenodd"
-          clipRule="evenodd"
-          className="fill-current"
-          alt=""
-        />
-        <p>
-          Blood Saver Ltd.
-          <br />
-          Donate blood to save life
-        </p>
-      </div>
-      <div>
-        <span className="font-bold">Quick Links</span>
-        <a className="link link-hover">About Us</a>
-        <a className="link link-hover">Charity</a>
-        <a className="link link-hover">FAQ</a>
-      </div>
-      <div>
-        <span className="font-bold">SAVELIFE</span>
-        <a className="link link-hover">Register</a>
-        <a className="link link-hover">Find Blood</a>
-        <a className="link link-hover">Live Feed</a>
-        <a className="link link-hover">Tracker</a>
-      </div>
-      <div>
-        <span className="font-bold">Services</span>
-        <a className="link link-hover">Blood Donation</a>
-        <a className="link link-hover">Health Check</a>
-        <a className="link link-hover">Blood Bank</a>
-        <a className="link link-hover">Donate Process</a>
-      </div>
-    </footer>
-  );
-};
-
-export default Footer;
-
- */
