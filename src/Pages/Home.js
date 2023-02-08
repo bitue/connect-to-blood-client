9fe03497d510ffcbc2bd11935973c9e00966f1bb
dev-naime <naime2molla@gmail.com>
2023-02-08 20:24:23 +0600

    news section added

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import About from "../Components/Home/About";
 import Hero from "../Components/Home/Hero";
 import InfoCards from "../Components/Home/InfoCards";
+import News from "../Components/Home/News";
 import Navbar from "../Layout/Navbar/Navbar";
 import Stats from "./Stats/Stats";
 
@@ -13,6 +14,7 @@ const Home = () => {
       <About />
       <InfoCards />
       <Stats />
+      <News />
     </div>
   );
 };
