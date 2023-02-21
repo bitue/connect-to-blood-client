0c336ca7d2aefaf32ae83d5e51ded233dad565f2
hrid620 <hridishere@gmail.com>
2023-02-21 23:30:44 +0600

    lle

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,15 +1,17 @@
-import { createContext } from 'react';
-import { useToken } from '../Hooks/useToken';
+import { createContext } from "react";
+import { useToken } from "../Hooks/useToken";
 
 export const AuthContext = createContext();
 
-const AuthProvider = ({ children }) => {
-    const { user, setUser, saveToken } = useToken(); // get user info from this hooks
-    // if you add any context value here just make a custom hooks then provide it the the value object
-    console.log(user);
-    return (
-        <AuthContext.Provider value={{ user, setUser, saveToken }}>{children}</AuthContext.Provider>
-    );
+const Authprovider = ({ children }) => {
+  const { user, setUser, saveToken } = useToken(); // get user info from this hooks
+  // if you add any context value here just make a custom hooks then provide it the the value object
+  console.log(user);
+  return (
+    <AuthContext.Provider value={{ user, setUser, saveToken }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
-export default AuthProvider;
+export default Authprovider;
