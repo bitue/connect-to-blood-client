6dd382185bd98e0a9fee1b5b152a3b335c1913a9
AbdurrahmanTalha <abdurrahmantalha.dev@gmail.com>
2023-03-01 11:08:25 +0600

    chanage

--- src/Components/Dashboard/ApproveDonor.js
+++ src/Components/Dashboard/ApproveDonor.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../../context/AuthProvider';
 
 const ApproveDonor = () => {
+    const [donorRequest, setDonorRequest] = useState([])
+    const { token } = useContext(AuthContext)
+
+    const fetchData = () => {
+        axios.get("https://pear-gifted-lamb.cyclic.app/admin/allDonorReq", {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`
+            }
+        }).then(data => setDonorRequest(data.data))
+            .catch(error => console.log(error))
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+    console.log(donorRequest)
+
+
     return (
         <div>
             <h2 className="text-center text-3xl my-[20px]">Approve Donor</h2>
             <div class="overflow-x-auto px-[10px]">
                 <table class="table w-full">
                     <thead className="bg-[#fff]">
-                        <tr>
+                        <tr className="">
                             <th className="bg-[#222] text-[#f1f1f1]"></th>
                             <th className="bg-[#222] text-[#f1f1f1]">Name</th>
                             <th className="bg-[#222] text-[#f1f1f1]">Email</th>
@@ -20,62 +41,28 @@ const ApproveDonor = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr className="border-[#222]">
-                            <th>1</th>
-                            <td>Cy Ganderton</td>
-                            <td>Cyganderton@gmail.com</td>
-                            <td>True</td>
-                            <td>A+</td>
-                            <td>False</td>
-                            <td>False</td>
-                            <td>
-                                <button class="btn no-animation gap-2 bg-green-500 mr-[14px] hover:bg-primary hover:border-primary text-white">
-                                    Approve
-                                </button>
-                            </td>
-                            <td>
-                                <button class="btn no-animation gap-2 bg-primary hover:bg-primary hover:border-primary  text-white">
-                                    Deny
-                                </button>
-                            </td>
-                        </tr>
-                        <tr className="border-[#222]">
-                            <th>1</th>
-                            <td>Cy Ganderton</td>
-                            <td>Cyganderton@gmail.com</td>
-                            <td>True</td>
-                            <td>A+</td>
-                            <td>False</td>
-                            <td>False</td>
-                            <td>
-                                <button class="btn no-animation gap-2 bg-green-500 mr-[14px] hover:bg-primary hover:border-primary  text-white">
-                                    Approve
-                                </button>
-                            </td>
-                            <td>
-                                <button class="btn no-animation gap-2 bg-primary hover:bg-primary hover:border-primary  text-white">
-                                    Deny
-                                </button>
-                            </td>
-                        </tr>
-                        <tr className="border-[#222]">
-                            <th>1</th>
-                            <td>Cy Ganderton</td>
-                            <td>Cyganderton@gmail.com</td>
-                            <td>True</td>
-                            <td>A+</td>
-                            <td>False</td>
-                            <td>False</td>
-                            <td><button class="btn no-animation gap-2 bg-green-500 mr-[14px] hover:bg-primary hover:border-primary  text-white">
-                                Approve
-                            </button>
-                            </td>
-                            <td>
-                                <button class="btn no-animation gap-2 bg-primary hover:bg-primary hover:border-primary  text-white">
-                                    Deny
-                                </button>
-                            </td>
-                        </tr>
+                        {
+                            donorRequest.map((user, index) => <tr className="border-[#222]">
+                                <th>{index + 1}</th>
+                                <td>Cy Ganderton</td>
+                                <td>Cyganderton@gmail.com</td>
+                                <td>{user.hiv}</td>
+                                <td>A+</td>
+                                <td>{user.smoke}</td>
+                                <td>False</td>
+                                <td>False</td>
+                                <td>
+                                    <button class="btn no-animation gap-2 bg-green-500 mr-[14px] hover:bg-primary hover:border-primary text-white">
+                                        Approve
+                                    </button>
+                                </td>
+                                <td>
+                                    <button class="btn no-animation gap-2 bg-primary hover:bg-primary hover:border-primary  text-white">
+                                        Deny
+                                    </button>
+                                </td>
+                            </tr>)
+                        }
 
                     </tbody>
                 </table>
