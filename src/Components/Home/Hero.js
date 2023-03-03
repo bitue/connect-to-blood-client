f00443c7e3a1483d30bf22e6cf5745a08650925b
hrid620 <hridishere@gmail.com>
2023-03-03 11:48:36 +0600

    blogs details like fixed for presentation

--- src/Components/Home/Hero.js
+++ src/Components/Home/Hero.js
@@ -1,21 +1,28 @@
-import React from 'react';
-import img from "../../Assets/images/hero-image.png"
+import React from "react";
+import img from "../../Assets/images/hero-image.png";
 
 const Hero = () => {
-    return (
-        <div className="hero pt-[30px]">
-            <div className="hero-content flex-col lg:flex-row-reverse">
-                <img src={img} className="rounded-lg md:w-[50%]" alt="hero" />
-                <div className="lg:w-[50%]">
-                    <h1 className="text-5xl font-bold lg:md:text-left text-center">Be a <span className="text-primary">Hero</span>, Donate Blood!</h1>
-                    <p className="py-6 text-1xl font-normal text-[#555] lg:md:text-left text-center">Connect to Blood connects donors with local hospitals. Give the gift of life and help save lives in your community. Join us today.</p>
-                    <div className="lg:md:block flex justify-center items-center">
-                        <button className="btn btn-outline border-secondary text-black border-2 hover:bg-primary hover:border-primary">Become a donar!</button>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="hero pt-[30px]">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <img src={img} className="rounded-lg md:w-[50%]" alt="hero" />
+        <div className="lg:w-[50%]">
+          <h1 className="text-5xl font-bold lg:md:text-left text-center">
+            Be a <span className="text-primary">Hero</span>, Donate Blood!
+          </h1>
+          <p className="py-6 text-1xl font-normal text-[#555] lg:md:text-left text-center">
+            Connect to Blood connects donors with local hospitals. Give the gift
+            of life and help save lives in your community. Join us today.
+          </p>
+          <div className="lg:md:block flex justify-center items-center">
+            <button className="btn btn-outline border-secondary text-black border-2 hover:bg-primary hover:border-primary">
+              Become a donar!
+            </button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
--- src/Pages/BlogsDetails.js
+++ src/Pages/BlogsDetails.js
@@ -89,7 +89,7 @@ const BlogsDetails = () => {
   };
 
   const { title, author, userEmail, createdAt, img, content, comments } = blog;
-
+  console.log(blog);
   return (
     <>
       <Navbar />
@@ -103,7 +103,7 @@ const BlogsDetails = () => {
           />
           <div className="flex justify-between items-center">
             <p className="text-sm text-secondary mr-[30px]">
-              {userEmail?.email?.split("@")[0]}
+              {blog?.user?.email?.split("@")[0]}
             </p>
             <p className="text-sm text-secondary">{createdAt?.split("T")[0]}</p>
           </div>
@@ -124,9 +124,8 @@ const BlogsDetails = () => {
           <div className="flex items-center">
             <button onClick={handleComment} className="mr-[10px]">
               <FontAwesomeIcon icon={faComment} className="h-4 w-4 mr-2" />
-              Comments
+              {comments?.length || 0} Comments
             </button>
-            <span>{comments?.length || 0} Comments</span>
           </div>
         </div>
         <div class="mt-4 bg-white rounded-md shadow-lg p-4">
@@ -168,7 +167,9 @@ const BlogsDetails = () => {
           {comments &&
             comments.map((comment) => (
               <div class="p-4 my-4 bg-gray-100 rounded-md" key={comment._id}>
-                <p class="text-gray-600 text-sm">{comment.comment}</p>
+                <div class="flex items-center mb-2">
+                  <p class="text-gray-600 text-sm">{comment.comment}</p>
+                </div>
               </div>
             ))}
         </div>
